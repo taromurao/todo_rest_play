817757f0c4b6a2e0a6bcb8c56b19705665ddf126
Taro Murao <taro.murao@gmail.com>
2018-02-15 08:45:58 +0100

    truncate tables after each todo integration test

--- test/integration/BaseIT.scala
+++ test/integration/BaseIT.scala
@@ -1,30 +1,37 @@
 package integration
 
+import java.sql.Connection
 import java.util.UUID
 
 import models.Email
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterEachTestData}
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.libs.ws._
 import repositories.UserRepository
+import anorm._
+import play.api.db.Database
 
-abstract class BaseIT extends PlaySpec with GuiceOneServerPerSuite with BeforeAndAfterAll{
+abstract class BaseIT extends PlaySpec with GuiceOneServerPerSuite with BeforeAndAfter{
 
   val A_PASSWORD = "1234567890"
   val AN_EMAIL: Email = "john@example.com"
   val A_USER_ID: UUID = UUID.fromString("d824a403-d159-4dcd-966c-0096274cd612")
 
+  protected val userRepository: UserRepository = app.injector.instanceOf[UserRepository]
   protected val wsClient: WSClient = app.injector.instanceOf[WSClient]
+  protected val db: Database = app.injector.instanceOf[Database]
   protected val host: String = s"http://localhost:$port"
 
-
-  override def beforeAll() {
+  before {
     createUser()
   }
 
-  protected def createUser(): Unit = {
-    val userRepository: UserRepository = app.injector.instanceOf[UserRepository]
-    userRepository.create(A_USER_ID, AN_EMAIL, A_PASSWORD)
+  after {
+    db.withConnection { implicit conn: Connection =>
+      SQL("TRUNCATE TABLE users CASCADE").execute()
+    }
   }
+
+  protected def createUser(): Unit = { userRepository.create(A_USER_ID, AN_EMAIL, A_PASSWORD) }
 }
