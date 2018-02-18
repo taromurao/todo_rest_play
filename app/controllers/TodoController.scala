1538c951227a88b8fbfff1ea87b12616d227d7bc
Taro Murao <taro.murao@gmail.com>
2018-02-18 07:59:28 +0100

    extracted withUser method

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -3,12 +3,11 @@ package controllers
 import java.util.UUID
 import javax.inject._
 
-import models.Todo
+import models.{Todo, User}
 import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc._
 import play.api.libs.functional.syntax._
-
 import repositories.{TodoRepository, UserRepository}
 
 import scala.util.{Success, Try}
@@ -22,45 +21,42 @@ class TodoController @Inject()(
   private val BAD_REQUEST_RESPONSE = BadRequest(JsObject(Seq("response" -> JsString("failed"))))
 
   def index(userId: UUID) = Action { implicit request =>
-    Logger.info(s"Executing TodoController.index")
-    userRepository.get(userId) match {
-      case Some(user) => {
-        Logger.info(s"Got user ${user}")
-        Try(todoRepository.ofUser(user)) match {
-          case Success(xs) => Ok(JsObject(Seq("response" -> JsString("ok"), "data" -> Json.toJson(xs))))
-          case _ => InternalServerError(JsObject(Seq("response" -> JsString("Database error"))))
-        }
+    withUser(userId) { user =>
+      Logger.info(s"Got user ${user}")
+      Try(todoRepository.ofUser(user)) match {
+        case Success(xs) => Ok(JsObject(Seq("response" -> JsString("ok"), "data" -> Json.toJson(xs))))
+        case _ => InternalServerError(JsObject(Seq("response" -> JsString("Database error"))))
       }
-      case _ => BAD_REQUEST_RESPONSE
     }
   }
 
   def create(userId: UUID): Action[JsValue] = Action(parse.json) { implicit request: Request[JsValue] =>
-    userRepository.get(userId) match {
-      case Some(user) => {
-        request.body.validate[(String, String)] map {
-          case (title, content) => {
-            todoRepository.save(user, title, content)
-            Created(JsObject(Seq("response" -> JsString("created"))))
-          }
-          case _ => BAD_REQUEST_RESPONSE
-        } recoverTotal { _ => BAD_REQUEST_RESPONSE }
-      }
-      case _ => BAD_REQUEST_RESPONSE
+    withUser(userId) { user =>
+      request.body.validate[(String, String)] map {
+        case (title, content) => {
+          todoRepository.save(user, title, content)
+          Created(JsObject(Seq("response" -> JsString("created"))))
+        }
+        case _ => BAD_REQUEST_RESPONSE
+      } recoverTotal { _ => BAD_REQUEST_RESPONSE }
     }
   }
 
   def update(userId: UUID, todoId: UUID): Action[JsValue] = Action(parse.json) { implicit request: Request[JsValue] =>
+    withUser(userId) { user: User =>
+      request.body.validate[Todo] map {
+        case t@Todo(_, _, _) => {
+          todoRepository.update(user, t)
+          Ok(JsObject(Seq("response" -> JsString("updated"))))
+        }
+        case _ => BAD_REQUEST_RESPONSE
+      } recoverTotal { _ => BAD_REQUEST_RESPONSE }
+    }
+  }
+
+  private def withUser(userId: UUID)(f: User => Result): Result = {
     userRepository.get(userId) match {
-      case Some(user) => {
-        request.body.validate[Todo] map {
-          case t @ Todo(_, _, _) => {
-            todoRepository.update(user, t)
-            Ok(JsObject(Seq("response" -> JsString("updated"))))
-          }
-          case _ => BAD_REQUEST_RESPONSE
-        } recoverTotal { _ => BAD_REQUEST_RESPONSE }
-      }
+      case Some(user) => f(user)
       case _ => BAD_REQUEST_RESPONSE
     }
   }
